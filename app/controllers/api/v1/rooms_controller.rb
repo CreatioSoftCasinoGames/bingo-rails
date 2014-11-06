1f566681f307145e8088b32ff329e3fa17034d58
Preeti Gupta <guptapreeti.india@gmail.com>
2014-11-07 02:25:44 +0530

    Added rooms indes api

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::RoomsController < Api::V1::ApplicationController
 
+	def rooms
+		render json: Room.all
+	end
+
 	def show
 		@room = Room.where(id: params[:id]).first
 		render json: @room

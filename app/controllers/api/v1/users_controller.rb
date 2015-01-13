29e515f832303077ee41e7738f59909db2ec56d6
Prashant Mishra <prashant.mishra@w3villa.com>
2015-01-13 17:34:26 +0530

    Remove user_not_found.html.slim used to show error message when user not found

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -52,9 +52,8 @@ class Api::V1::UsersController < Api::V1::ApplicationController
 	end
 
 	def find_user
-		@user = User.find(params[:id])
-		rescue
-		  render :template => 'application/user_not_found', :status => :not_found
+		@user = User.where(id: params[:id]).first
+		(render json: {message: "User not found", success: false}) if @user.blank?
 	end
 
 end

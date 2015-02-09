6a959eb45a341a7a4eb63278d93520292c954c42
Prashant Mishra <prashant.mishra@w3villa.com>
2015-02-09 12:32:14 +0530

    Remove unwanted attributes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150206154334) do
+ActiveRecord::Schema.define(version: 20150209065715) do
 
   create_table "api_keys", force: true do |t|
     t.string   "token"
@@ -73,10 +73,7 @@ ActiveRecord::Schema.define(version: 20150206154334) do
     t.integer  "num_cards"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "active",        default: true
-    t.integer  "resource_id"
-    t.string   "resource_type"
-    t.string   "bundle_type"
+    t.boolean  "active",      default: true
     t.string   "uuid"
   end
 

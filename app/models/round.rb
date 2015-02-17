f333a75c47b25c0aab47fa4ee6a0913db5dd5b05
Prashant Mishra <prashant.mishra@w3villa.com>
2015-02-17 15:19:43 +0530

    Optimize round model and user controller code

--- app/models/round.rb
+++ app/models/round.rb
@@ -23,35 +23,31 @@ class Round < ActiveRecord::Base
   	data.each do |node_obj|
   		round_user = round_users.where(user_id: node_obj['playerId']).first
   		user = round_user.user
-      score = 2*node_obj['daubs'].to_f + 10*node_obj['bingo'].to_f
+      scor = 2*node_obj['daubs'].to_f + 10*node_obj['bingo'].to_f
+      round_number = (node_obj['round'] <= 3) ? node_obj['round'] : 1
   		round_users_attributes.push({
   			id: round_user.id,
   			daubs: node_obj['daubs'],
   			bingos: node_obj['bingo'],
         room_id: node_obj['room_id'],
         attempt_number: node_obj['attempt_number'],
-        round_number: node_obj['round'],
-        score: score
+        round_number: round_number,
+        score: scor
   		})
   		bingo_played = user.bingo_played.to_f + 1
   		total_daubs = user.total_daubs.to_f + node_obj['daubs'].to_f
-      coins = 2*node_obj['daubs'].to_f + 10*node_obj['bingo'].to_f
   		users_attributes.push({
   			id: user.id,
   			total_daubs: total_daubs,
   			bingo_played: bingo_played,
         tickets_purchased: node_obj['cards'],
-        coins: user.try(:coins).to_f + coins
+        coins: user.try(:coins).to_f + scor
   		})
       tournament_user = TournamentUser.where(tournament_id: self.resource_id, user_id: user.id).first
-      p self.resource_id
-      round_score = 2*node_obj['daubs'].to_f + 10*node_obj['bingo'].to_f
-      total_score = RoundUser.where(room_id: node_obj['room_id'], round_number: node_obj['round'], user_id: user.id).pluck(:score).max().to_f
-      score_get = (round_score > total_score) ? round_score : total_score
-      p score_get
+      total_score = RoundUser.where(room_id: node_obj['room_id'], round_number: round_number, user_id: user.id).pluck(:score).max().to_f
+      score_get = (scor > total_score) ? scor : total_score
       score = tournament_user.try(:score).to_f + score_get.to_f
       over = (node_obj['round'] >= 3)
-      p over
       tournament_users_attributes.push({
         id: tournament_user.try(:id),
         user_id: user.id,
--- app/workers/rounds_worker.rb
+++ app/workers/rounds_worker.rb
@@ -5,8 +5,8 @@ class RoundsWorker
 
 	def perform(data)
 		k = JSON.parse(data)
-		k["resource_id"] = Tournament.where(room_id: k["room_id"]).last.id
 		k["resource_type"] = k["resource_type"].capitalize
+		k["resource_id"] = Tournament.where(room_id: k["room_id"]).last.id
 		Round.create(k)
 	end
 

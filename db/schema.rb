3d8b8c4ae11f24915c41d7242342b944c907a2ed
Prashant Mishra <prashant.mishra@w3villa.com>
2015-03-12 11:34:33 +0530

    Add is_bot field in users scheema.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226073922) do
+ActiveRecord::Schema.define(version: 20150312060222) do
 
   create_table "api_keys", force: true do |t|
     t.string   "token"
@@ -198,6 +198,7 @@ ActiveRecord::Schema.define(version: 20150226073922) do
     t.string   "device_id"
     t.integer  "bingo_by_corner_pattern",                               default: 0
     t.string   "login_token"
+    t.boolean  "is_bot",                                                default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

4d343f7a8f9d9ee58ed5279c9647aadba446f19f
Ishank Gupta <ishank_18@yahoo.com>
2015-03-31 17:46:34 +0530

    Refactored tournament mark_as_end method

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -8,44 +8,28 @@ class Tournament < ActiveRecord::Base
   accepts_nested_attributes_for :tournament_users
 
   def self.mark_as_end
-    #End and start a daily bingo tournament
-  	current_daily_tournament = self.where(active: true, tournament_type: "daily").last
-  	current_daily_tournament.tournament_users.order("score DESC").limit(3).each_with_index do |tournament_user, i|
-  		Reward.create(tournament_id: tournament_user.tournament_id, user_id: tournament_user.user_id, rank: i+1)
-  	end
-  	current_daily_tournament.update_attributes(active: false)
-  	Tournament.create(room_id: current_daily_tournament.room_id, active: true, tournament_type: "daily")
+    Tournament.create_tournament("Daily_Free", 1)
+    Tournament.create_tournament("Weekly", 7)
+    Tournament.create_tournament("Monthly", 30)
+  end
 
-    #End and start a weekly bingo tournament
-    weekly_tournaments = self.where(active: true, tournament_type: "weekly")
-    if weekly_tournaments.present?
-      current_weekly_tournament = weekly_tournaments.select {|weekly_tournament| weekly_tournament.created_at.to_date == (Time.now - 7.day).to_date}.first
-      if current_weekly_tournament.present?
-        current_weekly_tournament.tournament_users.order("score DESC").limit(3).each_with_index do |tournament_user, i|
-          Reward.create(tournament_id: tournament_user.tournament_id, user_id: tournament_user.user_id, rank: i+1)
-        end
-        current_weekly_tournament.update_attributes(active: false)
-      end
-      Tournament.create(room_id: weekly_tournaments.last.room_id, active: true, tournament_type: "weekly")
-    # else
-    #   Tournament.create(room_id: weekly_tournaments.last.room_id, active: true, tournament_type: "weekly")
+  def self.create_tournament(room_config_name, num_days)
+    room_config = RoomConfig.where(name: room_config_name).first
+    tournaments = room_config.tournaments.where(active: true)
+    if tournaments.present?
+      genrate_rewards(type, num_days)
     end
+    Tournament.create(room_config_id: RoomConfig.where(name: room_type).first.id, active: true, tournament_type: room_type)
+  end
 
-    #End and start a weekly bingo tournament
-    monthly_tournaments = self.where(active: true, tournament_type: "monthly")
-    if monthly_tournaments.present?
-      current_monthly_tournament = monthly_tournaments.select {|monthly_tournament| monthly_tournament.created_at.to_date == (Time.now - 30.day).to_date}.first
-      if current_monthly_tournament.present?
-        current_monthly_tournament.tournament_users.order("score DESC").limit(3).each_with_index do |tournament_user, i|
-          Reward.create(tournament_id: tournament_user.tournament_id, user_id: tournament_user.user_id, rank: i+1)
-        end
-        current_monthly_tournament.update_attributes(active: false)
+  def self.genrate_rewards(tournaments, num_days)
+    current_tournament = tournaments.select {|weekly_tournament| weekly_tournament.created_at.to_date == (Time.now - num_days.days).to_date}.first
+    if current_tournament.present?
+      current_tournament.tournament_users.order("score DESC").limit(3).each_with_index do |tournament_user, i|
+        Reward.create(tournament_id: tournament_user.tournament_id, user_id: tournament_user.user_id, rank: i+1)
       end
-      Tournament.create(room_id: monthly_tournaments.last.room_id, active: true, tournament_type: "monthly")
-    # else
-    #   Tournament.create(room_id: monthly_tournaments.last.room_id, active: true, tournament_type: "monthly")
+      current_tournament.update_attributes(active: false)
     end
-
   end
 
 end

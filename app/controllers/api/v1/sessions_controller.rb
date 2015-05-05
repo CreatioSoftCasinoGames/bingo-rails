3bdcf9ba2dbafb96af692480e9894683c95baa24
Prashant Mishra <prashant.mishra@w3villa.com>
2015-05-05 11:28:06 +0530

    Added image url in user profile and game data.

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -67,7 +67,7 @@ class Api::V1::SessionsController < Api::V1::ApplicationController
 				@user.previous_login_token = @user.login_histories.order("created_at desc").limit(2).last.try(:login_token)
 				render json: @user.as_json({
 					only: [:login_token, :previous_login_token, :first_name, :last_name, :email, :total_daubs, :tokens, :coins, :keys, :xp_earned, :current_level, :total_bingo, :total_card_used, :powerups_used, :total_jigsaw_completed, :jigsaw_data_string, :achievement_data_string, :total_free_spin_count, :total_scratch_count, :daily_bonus_time_remaining, :special_reward_timer, :ticket_bought],
-					methods: [:num_friend_request, :num_gift_request, :player_since],
+					methods: [:num_friend_request, :num_gift_request, :player_since, :image_url],
 					include: [:powerup, :in_app_purchases]
 				})
 			else

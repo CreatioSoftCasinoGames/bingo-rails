a57faca258614a58f346bfd2ce9dedf5e64bd87c
Ishank Gupta <ishank_18@yahoo.com>
2015-05-10 14:23:20 +0530

    Fixed migration

--- db/migrate/20150505121914_delete_total_bingo_from_users.rb
+++ db/migrate/20150505121914_delete_total_bingo_from_users.rb
@@ -1,5 +1,9 @@
 class DeleteTotalBingoFromUsers < ActiveRecord::Migration
-  def change
-  	remove_column :users, :total_bing
+  def self.up
+  	remove_column :users, :total_bingo
+  end
+
+  def self.down
+  	add_column :users, :total_bingo, :integer
   end
 end

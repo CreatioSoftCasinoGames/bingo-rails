88d6ebb24e3768367ff5df1caa8c95909a725366
Amrendra Singh <amrendra.singh@w3villa.com>
2015-05-27 19:08:24 +0530

    Link to client bug, remove logs

--- app/mailers/notify_admin_user.rb
+++ app/mailers/notify_admin_user.rb
@@ -2,9 +2,7 @@ class NotifyAdminUser < ActionMailer::Base
   default from: "bugs@creatiosoft.com"
 
   def send_error_mail(error)
-  	p "----------------------Called NotifyAdminUser---------------------"
-  	p error.exception
-  	mail(to: "rishabh@creatiosoft.com", subject: error.exception)
+  	mail(to: "rishabh@creatiosoft.com", subject: error.bug_type, body: error.exception)
   end
 
 end

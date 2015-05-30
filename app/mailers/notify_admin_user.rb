51ed80a24de22b1c65fb98d9e22655a9cec12adf
Ishank Gupta <ishank_18@yahoo.com>
2015-05-30 17:52:01 +0530

    Added email setting

--- app/mailers/notify_admin_user.rb
+++ app/mailers/notify_admin_user.rb
@@ -2,11 +2,11 @@ class NotifyAdminUser < ActionMailer::Base
   default from: "bugs@creatiosoft.com"
 
   def send_error_mail(error)
-  	@localRecivers = "rishabh@creatiosoft.com, neeraj@creatiosoft.com"
-  	# @productionRecievers = "amrendra@creatiosoft.com, rishabh@creatiosoft.com, narendra@creatiosoft.com, jagadheeswarreddy@creatiosoft.com, manishkumar@creatiosoft.com"
+  	# @localRecivers = "rishabh@creatiosoft.com, neeraj@creatiosoft.com"
+  	@productionRecievers = "amrendra@creatiosoft.com, rishabh@creatiosoft.com, narendra@creatiosoft.com, jagadheeswarreddy@creatiosoft.com, manishkumar@creatiosoft.com"
   	@subject 	= "Error while - "+error.bug_type
   	@body 		=	"The following message will help you more to track the issue - \n\n"+error.exception
-  	mail(to: @localRecivers, subject: @subject, body: @body)
+  	mail(to: @productionRecievers, subject: @subject, body: @body)
   end
 
 end

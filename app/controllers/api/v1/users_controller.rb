63bd996352862441e783b2b423f2592f8e77bdf1
Amrendra Singh <amrendra.singh@w3villa.com>
2015-06-11 19:34:17 +0530

    Fix issues of remaining time and tournament paid or not

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -100,14 +100,13 @@ class Api::V1::UsersController < Api::V1::ApplicationController
 				@reward = @user.rewards.where(is_collected: false, tournament_id: @tournament.id).first
 				remaining_time = @tournament.created_at - Time.now + @room_config.duration.day
 			end
-			p rank
 			render json: {
 				round_one: @round_scores.present? ? @round_scores[:round_one_score] : 0,
 				round_two: @round_scores.present? ? @round_scores[:round_two_score] : 0,
 				round_three: @round_scores.present? ? @round_scores[:round_three_score] : 0,
 				round_four: @round_scores.present? ? @round_scores[:round_four_score] : 0,
 				round_five: @round_scores.present? ? @round_scores[:round_five_score] : 0,
-				remaining_time: remaining_time.present? ? remaining_time : 0,
+				remaining_time: remaining_time.present? ? remaining_time : @room_config.tournaments.last.created_at - Time.now.utc + @room_config.duration.day,
 				rank: rank.present? && rank != 1 ? rank : 0,
 				is_over: @is_over.present? ? @is_over.over : false,
 				reward_collected: @reward.present? ? @reward.is_collected : true,

7278b90488a4b5551dfd9193c0312ffb380b64c1
Amrendra Singh <amrendra.singh@w3villa.com>
2015-06-18 18:54:12 +0530

    Implemented broadcast for maintaining a server

--- app/controllers/api/v1/schedule_maintenances_controller.rb
+++ app/controllers/api/v1/schedule_maintenances_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::ScheduleMaintenancesController < Api::V1::ApplicationController
 	def sm
-		REDIS_CLIENT.PUBLISH("server_maintenance", {message: "Server is going on maintenance after 30 minutes!"})
-		render json: {
-			sucess: true
-		}
+		if params[:schedule_time].present?
+			REDIS_CLIENT.PUBLISH("server_maintenance", {publish_type: "server_maintenance", message: "Server is going on maintenance at "+params[:schedule_time]+" !"}.to_json)
+		else
+			REDIS_CLIENT.PUBLISH("kick_from_game", {publish_type: "kick_from_game", message: "Server is going on Maintenance!!"}.to_json)
+		end
 	end
 end

722678b06d28367c273226553fca154610c978f2
Amrendra Singh <amrendra.singh@w3villa.com>
2015-07-07 19:45:04 +0530

    Default dynamic iap response set

--- app/controllers/api/v1/dynamic_iaps_controller.rb
+++ app/controllers/api/v1/dynamic_iaps_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::DynamicIapsController < Api::V1::ApplicationController
 
 	def index
-		render json: DynamicIap.where(iap_type: params[:iap_type].capitalize, currency: params[:currency].upcase, is_active: true)
+		@dynamic_iap = DynamicIap.where(iap_type: params[:iap_type].capitalize, currency: params[:currency].upcase)
+		if @dynamic_iap.present?
+			render json: DynamicIap.where(iap_type: params[:iap_type].capitalize, currency: params[:currency].upcase)
+		else
+			render json: DynamicIap.where(iap_type: params[:iap_type].capitalize, currency: "USD")
+		end
 	end
 
 end

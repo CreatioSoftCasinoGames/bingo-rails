c8d1b99a99644ee6c462f77af681627b5c49807c
Shashank Diwaker <amrendra.singh@w3villa.com>
2015-07-09 13:04:36 +0530

    Active depedency enable for dynamic iap

--- app/controllers/api/v1/dynamic_iaps_controller.rb
+++ app/controllers/api/v1/dynamic_iaps_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::DynamicIapsController < Api::V1::ApplicationController
 
 	def index
-		@dynamic_iap = DynamicIap.where(iap_type: params[:iap_type].capitalize, currency: params[:currency].upcase)
+		@dynamic_iap = DynamicIap.where(iap_type: params[:iap_type].capitalize, currency: params[:currency].upcase, is_active: true)
 		if @dynamic_iap.present?
-			render json: DynamicIap.where(iap_type: params[:iap_type].capitalize, currency: params[:currency].upcase)
+			render json: @dynamic_iap
 		else
-			render json: DynamicIap.where(iap_type: params[:iap_type].capitalize, currency: "USD")
+			render json: DynamicIap.where(iap_type: params[:iap_type].capitalize, currency: "USD", is_active: true)
 		end
 	end
 

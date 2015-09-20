7c12024a0b73ba6f9ac829856d358b782cf546b0
Mayank <mail2mayank@gmail.com>
2015-09-20 21:29:08 +0530

    changes to puma config with more workers and threads. also changed bingo-api app deployment path

--- config/puma.rb
+++ config/puma.rb
@@ -3,12 +3,13 @@
 # start puma with:
 # RAILS_ENV=production bundle exec puma -C ./config/puma.rb
 
-application_path = '/apps/bingo/current'
+application_path = '/apps/bingo-api/current'
 railsenv = 'production'
 directory application_path
 environment railsenv
 daemonize true
 pidfile "#{application_path}/tmp/pids/puma-#{railsenv}.pid"
 stdout_redirect "#{application_path}/log/puma-#{railsenv}.stdout.log", "#{application_path}/log/puma-#{railsenv}.stderr.log"
-threads 0, 16
-bind "unix:///tmp/slot_machine.sock"
+workers 2
+threads 100, 100
+bind "unix:///tmp/bingo-api.sock"

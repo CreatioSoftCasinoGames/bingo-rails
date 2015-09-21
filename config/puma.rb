91b75f96c740b2bb87f07dd011562777200de970
Mayank <mail2mayank@gmail.com>
2015-09-21 15:13:36 +0530

    change number of workers for bingo-api to 8

--- config/puma.rb
+++ config/puma.rb
@@ -10,6 +10,6 @@ environment railsenv
 daemonize true
 pidfile "#{application_path}/tmp/pids/puma-#{railsenv}.pid"
 stdout_redirect "#{application_path}/log/puma-#{railsenv}.stdout.log", "#{application_path}/log/puma-#{railsenv}.stderr.log"
-workers 2
+workers 8
 threads 100, 100
 bind "unix:///tmp/bingo-api.sock"
